2124b1743cc22b8b7fb11c74642fdc9fffb9f97b
Hamza <ousferk228@gmail.com>
2025-03-03 09:34:33 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -297,7 +297,7 @@ def webhook():
     return '', 200
 
 def set_telegram_webhook():
-    webhook_url = "https://hamza-1.onrender.com/webhook"
+    webhook_url = "https://hamza-drs4.onrender.com/webhook"
     url = f"https://api.telegram.org/bot{telegram_token}/setWebhook?url={webhook_url}"
     try:
         response = requests.get(url, timeout=10)
